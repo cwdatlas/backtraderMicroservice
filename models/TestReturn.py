c9e761c252b76ab9575c44e0776af357bc06acf3
cwdatlas <ascott@carroll.edu>
2023-10-31 12:44:47 -0600

    rebuilt as a dataclass

--- models/TestReturn.py
+++ models/TestReturn.py
@@ -1,21 +1,8 @@
+from pydantic.v1.dataclasses import dataclass
+
+
+@dataclass
 class TestReturn:
-    def __init__(self, sma, ema, ending_value):
-        self.sma = sma
-        self.ema = ema
-        self.ending_value = ending_value
-
-    def set_sma(self, sma):
-        self.sma = sma
-
-    def set_ema(self, ema):
-        self.ema = ema
-
-    def set_ending_value(self, ending_value):
-        self.ending_value = ending_value
-
-    def to_dict(self):
-        return {
-            'sma': self.sma,
-            'ema': self.ema,
-            'ending_value': self.endingValue
-        }
+    sma: int
+    ema: int
+    ending_value: float
