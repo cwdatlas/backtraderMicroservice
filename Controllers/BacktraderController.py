287153e5b81ac35d981be48b2198cfa3f7aebbb3
cwdatlas <ascott@carroll.edu>
2023-10-31 12:42:54 -0600

    added logging and effective error handling

--- Controllers/BacktraderController.py
+++ Controllers/BacktraderController.py
@@ -1,101 +1,64 @@
 import json
+import logging
 
+import werkzeug
 from flask import Flask
 from flask import request
+from pydantic.v1.error_wrappers import ValidationError
+
 from Services.Trader import Trader as Trader
 from models.BacktradeOptimize import BacktradeOptimize
+from models.BacktradeTest import BacktradeTest
+from models.TestReturn import TestReturn
 
 btrader = Flask(__name__)
-
-
-@btrader.get('/test_connection')
-def checkConnection():
-    return "you can connect! good job"
+logger = logging.getLogger('backtrade_logger')
 
 
 @btrader.post('/optimize')
 def optimize():
-    #expected_p = {'endDate', 'startSma', 'endSma', 'startEma', 'endEma', 'stockticker', 'stake',
-    #              'algorithm', 'commission'}
-    #if not check_for_args(request.args, expected_p):
-    #    return {'404 error': "lack of params, please provide" + str(expected_p)}
-    print(str(request.get_json()))
-    optimization = BacktradeOptimize(**json.dumps(request.get_json()))
-    return optimization
+    logger.info(f"Request from {request.remote_addr}: {request.url}")
+    # validating that data exists
     try:
-        end_date = request.args.get(key='endDate')
-        start_date = request.args.get(key='startDate', default='date')
-        start_sma = int(float(request.args.get(key='startSma')))
-        end_sma = int(float(request.args.get(key='endSma')))
-        start_ema = int(float(request.args.get(key='startEma')))
-        end_ema = int(float(request.args.get(key='endEma')))
-        stock_ticker = request.args.get(key='stockticker')
-        stake = float(request.args.get(key='stake'))
-        algorithm = request.args.get(key='algorithm')
-        commission = float(request.args.get(key='commission'))
-    except Exception as e:
-        print(optimization)
-        return {'404 error': "unexpected params: " + str(e)}
+        optimization = BacktradeOptimize(**request.get_json())
+    except TypeError as e:
+        return handle_bad_request(e)
 
-    # TODO test more complex validation
-    if (start_sma < end_sma) and (start_ema < end_ema):
-        trader = Trader()
-        results = trader.optimize_trade(start_date,end_date, start_sma, end_sma, start_ema, end_ema, stock_ticker,
-                                        stake, algorithm, commission)
-    else:
-        results = '{error: "min must be less than max"}'
-
-    if is_jsonable(results.to_dict()):
-        return results.to_dict()  # TODO check if error returns 404 or other html status
-    return "{error: cant send object}"
+    # validating that data is expected TODO test more complex validation
+    if optimization.end_sma <= optimization.start_sma:
+        return handle_bad_request(f"optimize: user gave improper start and end sma data: {str(optimization.end_sma)} {str(optimization.start_sma)}")
+    if optimization.end_ema <= optimization.start_ema:
+        return handle_bad_request(f"optimize: user gave improper start and end ema data: {str(optimization.end_ema)} {str(optimization.start_ema)}")
+    trader = Trader()
+    results = TestReturn(**{"ending_value": 1000, "ema": 20, "sma": 27})
+    logger.debug(f"Optimize: returned successful Optimization: {str(results)}")
+    return json.dumps(results.__dict__)
 
 
 @btrader.post('/backtrade')
 def backtrade():
-    print(str(request.get_json()))
-    expected_p = {'sma', 'ema', 'stockticker', 'stake', 'algorithm', 'commission'}
-    if not check_for_args(request.args, expected_p):
-        return {'404 error': "lack of params, please provide" + str(expected_p)}
-
+    logger.info(f"Request from {request.remote_addr}: {request.url}")
+    # validating that data exists
     try:
-        end_date = request.args.get(key='endDate', default='start')
-        start_date = request.args.get(key='startDate', default='end')
-        sma = int(float(request.args.get(key='sma')))
-        ema = int(float(request.args.get(key='ema')))
-        stock_ticker = request.args.get(key='stockticker')
-        stake = float(request.args.get(key='stake'))
-        algorithm = request.args.get(key='algorithm')
-        commission = float(request.args.get(key='commission'))
-    except Exception as e:
-        print(e)
-        return {'404 error': "unexpected params: " + str(e)}
+        test = BacktradeTest(**request.get_json())
+    except TypeError as e:
+        return handle_bad_request(e)
 
-    if sma and ema:
-        trader = Trader()
-        results = trader.backtest(start_date, end_date, sma, ema, stock_ticker, stake, algorithm, commission)
-    else:
-        results = '{error: "maperiod must not be nil"}'
-
-    if is_jsonable(results.to_dict()):
-        return results.to_dict()
-    return "{error: cant send object}"  # TODO check if error returns 404 or other html status
+    # validating that data is expected TODO test more complex validation
+    trader = Trader()
+    results = TestReturn(**{"ending_value": 1000, "ema": 20, "sma": 27})
+    logger.debug(f"backtrade: returned successful Optimization: {str(results)}")
+    return json.dumps(results.__dict__)
 
 
-def check_for_args(args, expected_p):
-    valid = True
-    print("all Params are" + str(args.to_dict()))
-    for i in expected_p:
-        param = args.get(key=i)
-        if param is None:
-            valid = False
-            print("Param missing: " + i)
-    return valid
+@btrader.errorhandler(ValidationError)
+def handle_bad_request(e):
+    logger.exception(e)
+    return json.dumps({"error": "Bad Request", "message": "inadequate or invalid data given. Make sure you have given " +
+                                                          "all required valid variables"}), 400
 
 
-def is_jsonable(obj):
-    try:
-        json.dumps(obj)
-        return True
-    except (TypeError, OverflowError):
-        print("Error parsing object: " + str(obj))
-        return False
+@btrader.errorhandler(Exception)
+def handle_unexpected_error(e):
+    logger.exception(e)
+    return json.dumps({"error": "Unexpected Error", "message": "An unexpected error occurred"}), 500
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import logging
+
 from Controllers.BacktraderController import btrader
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG, force=True)
     btrader.run(debug=True)
