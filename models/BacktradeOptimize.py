f8fb8406a67edadd8b7da13b5838bca699491045
cwdatlas <ascott@carroll.edu>
2023-11-16 23:14:08 -0700

    Fleshed out validation for start and end metics

--- models/BacktradeOptimize.py
+++ models/BacktradeOptimize.py
@@ -15,6 +15,7 @@ class BacktradeOptimize(BacktradeData):
         end_sma = self.end_sma
         start_ema = self.start_ema
         end_ema = self.end_ema
+
         if start_sma <= 0:
             raise TypeError('Starting SMA is 0 or lower, must be more than 0 and less than 101')
         if start_ema <= 0:
@@ -23,6 +24,16 @@ class BacktradeOptimize(BacktradeData):
             raise TypeError('Starting SMA is 101 or higher, must be more than 0 and less than 101')
         if start_ema > 100:
             raise TypeError('Starting EMA is 101 or higher, must be more than 0 and less than 101')
+
+        if end_sma <= 0:
+            raise TypeError('Ending SMA is 0 or lower, must be more than 0 and less than 101')
+        if end_ema <= 0:
+            raise TypeError('Ending EMA is 0 or lower, must be more than 0 and less than 101')
+        if end_sma > 100:
+            raise TypeError('Ending SMA is 101 or higher, must be more than 0 and less than 101')
+        if end_ema > 100:
+            raise TypeError('Ending EMA is 101 or higher, must be more than 0 and less than 101')
+
         if start_sma >= end_sma:
             raise TypeError('Starting SMA is larger or equal to ending SMA')
         if start_ema >= end_ema:
